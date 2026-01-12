04efd11b880e4a2c20407662b0423578dae940c0
LiosK <contact@mail.liosk.net>
2026-01-11 22:28:10 -0500

    test: add test case for current_arg()

--- examples/test_read_line.rs
+++ examples/test_read_line.rs
@@ -1,13 +1,27 @@
-use std::io;
+use std::{env, io};
 
 fn main() -> io::Result<()> {
+    let mut args: Vec<_> = env::args_os().skip(1).collect();
+    if args.is_empty() {
+        args.push("-".into());
+    }
+
+    let mut diamond = diamond_op::new();
+    assert!(diamond.current_arg().is_none());
+
     let mut i = 0;
     let mut buf = String::new();
-    let mut diamond = diamond_op::new();
     while diamond.read_line(&mut buf)? != 0 {
         print!("[{}] {}", i, buf);
+
+        let arg = diamond.current_arg().unwrap();
+        let pos = args.iter().position(|e| e == arg).unwrap();
+        args.drain(..pos);
+
         buf.clear();
         i += 1;
     }
+
+    assert!(diamond.current_arg().is_none());
     Ok(())
 }
